69e274440bd2a5edb19d8de8642c96c2494262ab
MengYangkov <phengmengheak@outlook.com>
2025-08-25 23:26:34 +0700

    Integrated React build into Express server and configured static file serving for production. Automated deployment with a single npm run deploy command to build frontend and launch backend.

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
 
 const planetRouter = require('./routes/planets/planets.router');
 const app = express();
@@ -9,6 +10,11 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','public')));
+
 app.use(planetRouter);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
 
 module.exports = app;
