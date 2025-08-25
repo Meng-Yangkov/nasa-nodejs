be48f854bd08ba7d71790bf41bf907150badfe0d
MengYangkov <phengmengheak@outlook.com>
2025-08-25 21:11:45 +0700

    Loading data destination from backend to front-end

--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,17 @@ const app = require('./app');
 const PORT = process.env.PORT || 8000; 
 
 const server = http.createServer(app);
+const { loadPlanets } = require("./models/planets.model");
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-app.listen(PORT, (req, res) => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+async function startServer(){
+  await loadPlanets();
+  app.listen(PORT, (req, res) => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+}
+
+startServer();
