c2a74a24c1494fa7983a284b3c26ae7c0b94cc44
MengYangkov <phengmengheak@outlook.com>
2025-08-28 23:08:56 +0700

    Added POST endpoint for scheduling new launches with mission metadata, customer assignment, and dynamic flight number generation

--- server/src/routes/launches/lauches.router.js
+++ server/src/routes/launches/lauches.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { httpGetAllLaunches } = require('./lauches.controller');
+const { httpGetAllLaunches, httpAddNewLaunch } = require('./lauches.controller');
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', httpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/',httpAddNewLaunch);
 
 module.exports = launchesRouter;
