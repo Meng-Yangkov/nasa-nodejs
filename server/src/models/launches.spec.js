7e5ec89b6ad37c9eea6c2cb80817d32d5f11c393
MengYangkov <phengmengheak@outlook.com>
2025-09-01 22:40:41 +0700

    install jest for write testing code api on node

---
+++ server/src/models/launches.spec.js
@@ -0,0 +1,13 @@
+describe('Test GET /launches', () => {
+  test('It should work with 200 response code', () =>{
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe('Test POST /launches', () => {
+  test('It should work with 200 response code',() =>{});
+
+  test('It should catch missing the required properties', () => {});
+  test('It should catch invalid date', () => {})
+})
