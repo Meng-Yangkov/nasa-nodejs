fdc6321360997608f64edf9ae011d65fd0f91489
MengYangkov <phengmengheak@outlook.com>
2025-09-02 00:17:31 +0700

    write test on post and validate the date validation

--- server/src/models/launches.spec.js
+++ server/src/models/launches.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest'); //-> for testing http
 const app = require('../app')
 
 describe('Test GET /launches', () => {
-  test('It should work with 200 response code', async () =>{
+  test('It should work with 200 response code', async () => {
     const response = await request(app)
       .get('/launches')
       .expect('Content-Type', /json/)
@@ -12,17 +12,30 @@ describe('Test GET /launches', () => {
 });
 
 describe('Test POST /launches', () => {
-  test('It should work with 201 response code', async () =>{
+
+  const completeLaunchData = {
+    mission: 'Being Software Engineer',
+    rocket : 'F-16 Fighting falcon',
+    target: 'New York City',
+    launchDate: ('November 04, 2030')
+  };
+  const LaunchDataWithoutDate = {
+    mission: 'Being Software Engineer',
+    rocket : 'F-16 Fighting falcon',
+    target: 'New York City',
+  };  
+  test('It should work with 201 response code', async () => {
     const response = await request(app)
       .post('/launches')
-      .send({
-        mission: 'Being Software Engineer',
-        rocket : 'F-16 Fighting falcon',
-        target: 'New York City',
-        launchDate: ('November 04, 2030')
-      })
+      .send(completeLaunchData)
       .expect('Content-Type', /json/)
       .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+    expect(response.body).toMatchObject(LaunchDataWithoutDate);
   });
 
   test('It should catch missing the required properties', () => {});
